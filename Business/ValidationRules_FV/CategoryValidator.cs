a03a51b32b38251af3943d3bcc8aefaee319e01e
J.Fırat <61869832+JFASOF@users.noreply.github.com>
2021-05-04 13:59:29 +0300

    Update CategoryValidator.cs

--- Business/ValidationRules_FV/CategoryValidator.cs
+++ Business/ValidationRules_FV/CategoryValidator.cs
@@ -14,7 +14,7 @@ namespace Business.ValidationRules_FV
         {
             RuleFor(x => x.CategoryName).NotEmpty().WithMessage("Kategori Adı Boş Geçilemez");
             RuleFor(x => x.CategoryDescription).NotEmpty().WithMessage("Açıklama Boş Geçilemez");
-            RuleFor(x => x.CategoryName).MinimumLength(3).WithMessage("En az 3 karakter olmalıdır");            RuleFor(x => x.CategoryName).MinimumLength(3).WithMessage("En az 3 karakter olmalıdır");
+            RuleFor(x => x.CategoryName).MinimumLength(3).WithMessage("En az 3 karakter olmalıdır"); 
             RuleFor(x => x.CategoryName).MaximumLength(20).WithMessage("En fazla 20 karakter olmalıdır");
             RuleFor(x=>x.CategoryDescription).MaximumLength(50).WithMessage("En fazla 50 karakter olmalıdır");
         }
