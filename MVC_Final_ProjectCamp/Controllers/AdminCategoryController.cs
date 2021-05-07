c21ada3f0d0d453fcefa7c3873a6cf9b365e3085
J.Fırat <61869832+JFASOF@users.noreply.github.com>
2021-05-07 22:38:23 +0300

    All Course Completed

---
+++ MVC_Final_ProjectCamp/Controllers/AdminCategoryController.cs
@@ -0,0 +1,49 @@
+﻿using Business.Concrete;
+using Business.ValidationRules_FV;
+using DataAccess.EntityFramework;
+using Entities.Concrete;
+using FluentValidation.Results;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_Final_ProjectCamp.Controllers
+{
+    public class AdminCategoryController : Controller
+    {
+        //EFCategoryDal Farklı çalışma alanlarından çalışabilmek için little touches .. :d
+        CategoryManager cm = new CategoryManager(new EFCategoryDal());
+        // GET: AdminCategory
+        public ActionResult Index()
+        {
+            var categoryValues = cm.fetchCategoryList();
+            return View(categoryValues);
+        }
+        [HttpGet]
+        public ActionResult AddCategory()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult AddCategory(Category p)
+        {
+            CategoryValidator categoryValidator = new CategoryValidator();
+            ValidationResult results = categoryValidator.Validate(p);
+            if (results.IsValid)
+            {
+                cm.CategoryAddBL(p);
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                foreach (var item in results.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                }
+            }
+            return View();
+        }
+    }
+}
